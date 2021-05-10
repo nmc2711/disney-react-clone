b45257f9be39d17b0c025fa236c8db4fa16c269b
hwangsanghan <nmc2711@naver.com>
2021-05-10 22:27:08 +0900

    프로젝트 설정 및 헤더 푸ㅜ시

---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" alt="로고 이미지" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/iu.jpeg" alt="프로필 이미지" />
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  height: 70px;
+  padding: 0 36px;
+  background-color: #090b13;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  margin-left: 25px;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+    img {
+      height: 20px;
+    }
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+      &:after {
+        content: "";
+        height: 2px;
+        background-color: #fff;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform: scaleX(0);
+        transform-origin: left center;
+        transition: all 0.3s;
+      }
+    }
+    &:hover {
+      span:after {
+        opacity: 1;
+        transform: scaleX(1);
+      }
+    }
+  }
+`;
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
