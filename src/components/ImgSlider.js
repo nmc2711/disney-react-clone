9c54b37c3dc924af80c103565624d9d07a9033b7
hwangsanghan <nmc2711@naver.com>
2021-05-10 23:01:58 +0900

    메인 영역 및 슬릭 슬라이더 추가

---
+++ src/components/ImgSlider.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import styled from "styled-components";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+function ImgSlider() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+  };
+
+  return (
+    <Carousel {...settings}>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/slider-badging.jpg" />
+      </Wrap>
+    </Carousel>
+  );
+}
+
+export default ImgSlider;
+
+const Carousel = styled(Slider)``;
+
+const Wrap = styled.div``;
